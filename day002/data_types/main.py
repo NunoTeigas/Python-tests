30904d49e6a6932ae6a6b1e701c68719345978d0
NunoTeigas <157012395+NunoTeigas@users.noreply.github.com>
2024-01-22 20:05:39 +0000

    Data types

---
+++ day002/data_types/main.py
@@ -0,0 +1,11 @@
+two_digit_number = input("Type a two digit number.\n")
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+
+#print(type(two_digit_number)) #this type function outputs the data type of the input
+
+a = int(two_digit_number[0])
+b = int(two_digit_number[1])
+
+print(f"The sum of the two digits is {a + b}.")
